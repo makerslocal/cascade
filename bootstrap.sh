9fe1ac9ef3c1bc9a73671361003172f3d1e16f48
clarkda <dusty@clarkda.com>
2014-07-07 23:49:02 -0500

    oops

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update &&
-sudo apt-get install build-essential sqlite3 libudev-dev
+sudo apt-get update
+sudo apt-get install -y build-essential sqlite3 libudev-dev
 
-sudo wget http://node-arm.herokuapp.com/node_latest_armhf.deb &&
-sudo dpkg -i node_latest_armhf.deb
+sudo wget http://node-arm.herokuapp.com/node_latest_armhf.deb && sudo dpkg -i node_latest_armhf.deb
 
 npm install --sqlite=/usr/bin
