f34e8ce1e6e0ebadd8425006f515003ff625a05c
clarkda <dusty@clarkda.com>
2014-09-06 21:32:49 -0500

    moved to scripts/

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get install -y build-essential sqlite3 libudev-dev
-
-sudo wget http://node-arm.herokuapp.com/node_latest_armhf.deb && sudo dpkg -i node_latest_armhf.deb
-
-npm install --sqlite=/usr/bin
